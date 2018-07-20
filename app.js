3147066e52c316e578eb3d3477c30a552dc6215c
Nick Doyle <nick.p.doyle@gmail.com>
2018-07-20 23:00:33 +1000

    feat: default theme united

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ if (cluster.isMaster) {
     app.get('/', function(req, res) {
         res.render('index', {
             static_path: 'static',
-            theme: process.env.THEME || 'flatly',
+            theme: process.env.THEME || 'united',
             flask_debug: process.env.FLASK_DEBUG || 'false'
         });
     });
